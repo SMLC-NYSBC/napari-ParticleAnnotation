4c885ac52829b5820a9ce71fbf5b8f4d3c7a6027
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2023-10-23 20:35:23 +0200

    up api

--- ParticleAnnotation/cloud/aws_api.py
+++ ParticleAnnotation/cloud/aws_api.py
@@ -1,4 +1,4 @@
-from os import listdir, mkdir
+from os import listdir, mkdir, makedirs
 from os.path import isdir
 from typing import List
 
@@ -8,13 +8,16 @@ from fastapi import FastAPI, HTTPException,  File, UploadFile
 
 app = FastAPI()
 url = 'http://3.236.232.251:8000/api'
+dir_ = "api/data/images/"
 
 
 @app.get("/listfiles/", response_model=List[str])
 async def list_files():
+    makedirs(dir_, exist_ok=True)
+
     try:
         # List all files in the predefined folder
-        files = listdir("api/data/images/")
+        files = listdir(dir_)
 
         if format:
             files = [file for file in files if file.endswith(f".{format}")]
@@ -25,10 +28,7 @@ async def list_files():
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    dir_ = "api/data/images/"
-
-    if not isdir(dir_):
-        mkdir(dir_)
+    makedirs(dir_, exist_ok=True)
 
     try:
         file_location = f"{dir_}/{file.filename}"
